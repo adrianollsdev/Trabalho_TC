f98167ea9ed1d4a398c9c2db84a2a9c7105797a8
Adriano Lopes Leão da Silva <adrianolls.dev@gmail.com>
2019-05-08 10:01:56 -0300

    Corrigindo problema com a posição

--- automaton_word_recognition.py
+++ automaton_word_recognition.py
@@ -10,52 +10,61 @@ class Automaton:
 
     def verify_acceptance(word, automato):
         i = 0
+        word = Automaton.clean_word(word)
         for letter in word:
             if i < len(word):
-                # print('letter: ', letter, 'automato[%d]: ' % i, automato[i])
                 if letter == automato[i]:
                     i = i + 1
-                    if i == len(word)-1 and i == len(automato)-1:
+                    if i == len(word) and i == len(automato):
+                        i = 0
                         return 1
                 else:
+                    i = 0
                     return 0
             else:
                 if i == len(word) and i == len(automato):
+                   i = 0
                    return 1
                 else:
+                   i = 0
                    return 0
 
     def run(key_word, words_texto, automato, out_info):
-        count_i = 0
-        count_text = 1
-        out_info.write('\nAutomato para a string: %s \n\n' % key_word)
+        count_i     = 0
+        count_text  = 1
+        count_print = 0
+        out_info.write('\nAutomato para validar a string: %s \n\n' % key_word)
         for word in words_texto:
+            word_print = Automaton.clean_word(word)
             flag_accept = Automaton.verify_acceptance(word, automato)
-            # print('flag_aceita: ', flag_accept)
             if(flag_accept == 1):
-                line = 'Palavra -> ' + str(word) + ' = Aceita......Começa na Posição: ' + str(count_text)
+                line = 'Palavra -> ' + str(word_print) + ' = Aceita......Começa na Posição: ' + str(count_text)
                 out_info.write(line +'\n')
+                count_text = count_text + len(word) + 1
+                count_print += 1
             else :
                 count_text = count_text + len(word) + 1
             count_i = count_i + 1
-        out_info.write('\nCaso não exista valor no intervalo acima é porque não existe a palavra chave no texto\n')
+        if count_print == 0:
+            out_info.write('Não há esta palavra no texto.\n')
         return 0
-        
+    
+    def clean_word(word):
+        return re.sub(r'[^\w\s]', '', word)
 
 
-in_text = open('..\\Trabalho_TC\\Input\\entrada_texto.txt', 'r+', encoding='UTF-8')
-in_key_words = open('..\\Trabalho_TC\\Input\\entrada_key_word.txt', encoding='UTF-8')
-out_info = open('..\\Trabalho_TC\\Output\\saida_dados.txt', 'a', encoding='UTF-8')
+in_text = open('..\\Trabalho_TC\\Input\\input_text.txt', encoding='UTF-8')
+in_key_words = open('..\\Trabalho_TC\\Input\\input_key_word.txt', encoding='UTF-8')
+out_info = open('..\\Trabalho_TC\\Output\\output_information.txt', 'a', encoding='UTF-8')
 
 
 text = in_text.readlines()
-in_text.write(' ')
 key_words = in_key_words.read().splitlines()
 count = 0
+words_texto = []
 
 for line in text:
-    line = re.sub(r'[^\w\s]', '', line)
-    words_texto = line.split(' ')
+    words_texto = words_texto + line.replace('\n', '').split(' ')
 
 for key_word in key_words:
     automato = Automaton.make(key_word)
